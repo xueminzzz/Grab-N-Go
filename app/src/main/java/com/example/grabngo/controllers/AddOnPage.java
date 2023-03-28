3dd9ed240d33cdf5ce92f8178a114fe9bc95fd72
mavene <dianmaisara@gmail.com>
2023-03-28 12:35:54 +0800

    fix add-on proceed button enable/disable toggle, add test package for design patterns, rename pages after removing duplicate

--- app/src/main/java/com/example/grabngo/controllers/AddOnPage.java
+++ app/src/main/java/com/example/grabngo/controllers/AddOnPage.java
@@ -13,16 +13,17 @@ import androidx.core.content.ContextCompat;
 
 import com.example.grabngo.R;
 
-public class AddOnPage1 extends Activity {
+public class AddOnPage extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.add_on_page_1);
+        setContentView(R.layout.add_on_page);
 
         CheckBox check1 = findViewById(R.id.MeatCheckBox);
         CheckBox check2 = findViewById(R.id.EggCheckBox);
         CheckBox check3 = findViewById(R.id.TofuCheckBox);
-        CheckBox check4 = findViewById(R.id.NoAddOnCheckBox);
+        CheckBox check4 = findViewById(R.id.CucumberCheckBox);
+        CheckBox check5 = findViewById(R.id.NoAddOnCheckBox);
 
         AppCompatButton proceedbtn = findViewById(R.id.PlaceOrderButton1);
 
@@ -32,7 +33,7 @@ public class AddOnPage1 extends Activity {
                 if (check1.isChecked()) {
                     proceedbtn.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.button_enabled));
                     proceedbtn.setEnabled(true);
-                } else {
+                } else if (!check2.isChecked() && !check3.isChecked() && !check4.isChecked() && !check5.isChecked()) {
                     proceedbtn.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.button_disabled));
                     proceedbtn.setEnabled(false);
                 }
@@ -45,7 +46,7 @@ public class AddOnPage1 extends Activity {
                 if (check2.isChecked()) {
                     proceedbtn.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.button_enabled));
                     proceedbtn.setEnabled(true);
-                } else {
+                } else if (!check1.isChecked() && !check3.isChecked() && !check4.isChecked() && !check5.isChecked()) {
                     proceedbtn.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.button_disabled));
                     proceedbtn.setEnabled(false);
                 }
@@ -58,7 +59,7 @@ public class AddOnPage1 extends Activity {
                 if (check3.isChecked()) {
                     proceedbtn.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.button_enabled));
                     proceedbtn.setEnabled(true);
-                } else {
+                } else if (!check1.isChecked() && !check2.isChecked() && !check4.isChecked() && !check5.isChecked()) {
                     proceedbtn.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.button_disabled));
                     proceedbtn.setEnabled(false);
                 }
@@ -71,7 +72,20 @@ public class AddOnPage1 extends Activity {
                 if (check4.isChecked()) {
                     proceedbtn.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.button_enabled));
                     proceedbtn.setEnabled(true);
-                } else {
+                } else if (!check1.isChecked() && !check2.isChecked() && !check3.isChecked() && !check5.isChecked()) {
+                    proceedbtn.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.button_disabled));
+                    proceedbtn.setEnabled(false);
+                }
+            }
+        });
+
+        check5.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (check5.isChecked()) {
+                    proceedbtn.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.button_enabled));
+                    proceedbtn.setEnabled(true);
+                } else if (!check1.isChecked() && !check2.isChecked() && !check3.isChecked() && !check4.isChecked()) {
                     proceedbtn.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.button_disabled));
                     proceedbtn.setEnabled(false);
                 }
@@ -81,7 +95,7 @@ public class AddOnPage1 extends Activity {
         proceedbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent nextAdd = new Intent(view.getContext(), AddOnPage2.class);
+                Intent nextAdd = new Intent(view.getContext(), ConfirmAddOnpPage.class);
                 view.getContext().startActivity(nextAdd);
             }
         });
