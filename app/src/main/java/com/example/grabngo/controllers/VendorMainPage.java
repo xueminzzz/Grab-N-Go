53fae5a014bd6bea3ed060e39ea83e5776399dfe
huiyi17 <65885651+huiyi17@users.noreply.github.com>
2023-03-30 20:33:22 +0800

    Update VendorMainPage.java

--- app/src/main/java/com/example/grabngo/controllers/VendorMainPage.java
+++ app/src/main/java/com/example/grabngo/controllers/VendorMainPage.java
@@ -3,6 +3,14 @@ package com.example.grabngo.controllers;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.ImageButton;
+import android.view.View.OnClickListener;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.grabngo.R;
 
@@ -19,10 +27,68 @@ import com.example.grabngo.R;
 // Java Concepts/OOP: Intent(?)
 
 public class VendorMainPage extends AppCompatActivity {
+    ImageButton button1230PM;
+    ImageButton button1PM;
+    ImageButton button130PM;
+    ImageButton button2PM;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_vendor_page);
+
+        button1230PM = (ImageButton) findViewById(R.id.Time1230PM);
+        button1PM = (ImageButton) findViewById(R.id.Time1PM);
+        button130PM = (ImageButton) findViewById(R.id.Time130PM);
+        button2PM = (ImageButton) findViewById(R.id.Time2PM);
+
+        button1230PM.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(VendorMainPage.this, OrdersforSpecifiedTimePage1.class);
+                startActivity(i);
+
+            }
+        });
+
+        button1PM.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(VendorMainPage.this, OrdersforSpecifiedTimePage1_1PM.class);
+                startActivity(i);
+
+            }
+        });
+
+        button130PM.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(VendorMainPage.this, OrdersforSpecifiedTimePage1_130PM.class);
+                startActivity(i);
+
+
+
+            }
+        });
+
+        button2PM.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(VendorMainPage.this, OrdersforSpecifiedTimePage1_2PM.class);
+                startActivity(i);
+
+
+            }
+        });
+
+
     }
 }
